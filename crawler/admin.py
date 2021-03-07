3e0ef611155c25e9adfa6a3df8e33f636f6e2e6d
SARIBEG10 <sfarmanian@gmail.com>
2021-03-07 13:56:53 +0330

    customize admin panel and last scraper commit

--- crawler/admin.py
+++ crawler/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
-
+from .models import Link
 # Register your models here.
+
+
+admin.site.site_header = "Crawler"
+admin.site.site_title = "scraper"
+admin.site.index_title = "link collector"
+
+
+class Crawler(admin.ModelAdmin):
+
+    def change_name_to_default(self, request, queryset):
+        queryset.update(name='default')
+
+    change_name_to_default.short_description = 'none to default'
+
+    list_display = ('name', 'address')
+    search_fields = ('address',)
+    actions = ('change_name_to_default',)
+    fields = ('address',)
+    list_editable = ('address',)
+
+
+admin.site.register(Link, Crawler)
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from crawler import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.scrape, name='scrape'),
+    path('delete/', views.clear, name='clear')
 ]
